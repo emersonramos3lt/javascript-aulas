fb7a4dedcc59d12eb5a1c4383e4d8e51069eb4cb
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2024-03-18 15:56:32 -0300

    New

---
+++ ex017-logical-operators/index.js
@@ -0,0 +1,38 @@
+// logical operators = used to combine or manipulate boolean values (true or false)
+
+// AND = && OR = || NOT = !
+
+/*
+const temp = 15;
+
+if(temp > 0 && temp <= 30) {
+    console.log("The weather is GOOD");
+}
+
+else {
+    console.log("The weather is BAD");
+}
+*/
+
+/*
+let temp = 25;;
+let clima = "sol";
+
+if (temp <= 25 || clima === "sol") {
+    console.log("Um bom dia para sair");
+}
+
+else {
+    console.log("Acho que hoje é melhor ficar em casa");
+}
+*/
+
+let chuva = true
+
+if (!chuva) {
+    console.log("Não está chovendo");
+}
+
+else {
+    console.log("Está chovendo");
+}
